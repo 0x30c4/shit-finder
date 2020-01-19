f68b45e78696d241e802256202c1aad27e89d78b
0x30c4 <40656876+0x30c4@users.noreply.github.com>
2020-01-19 23:09:32 +0600

    Install the program

--- setup.sh
+++ setup.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-mkdir .Shit-finder
-cp shit_finder.py ~/.Shit-finder
-echo alias findshit=\"python3 ~/.Shit-finder/shit_finder.py\" >> ~/.bashrc
+
+if [ ! -f "shit_finder.py" ]; then
+    wget https://raw.githubusercontent.com/0x30c4/shit-finder/master/shit_finder.py
+fi
+
+chmod +x shit_finder.py
+
+mv shit_finder.py shit_finder
+
+cp shit_finder ~/.local/bin
 
 echo 
 echo "------------------------------Shit-Finder-------------------------------"
 echo   Shit-Finder is installed successfully!!!!
+<<<<<<< HEAD
+printf "To use shit_finder type 'shit_finder --help'\n\n"
+=======
 printf "To use shit-finder restart your terminal and then type 'findshit --help'\n\n"
+>>>>>>> e212361d79d85d7850483d26d4aba3ecb583658a
