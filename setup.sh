797faa5a8ae9634df5e7d18e05098079ede61585
0x30c4 <40656876+0x30c4@users.noreply.github.com>
2020-01-19 23:17:31 +0600

    Install the program

--- setup.sh
+++ setup.sh
@@ -13,8 +13,6 @@ cp shit_finder ~/.local/bin
 echo 
 echo "------------------------------Shit-Finder-------------------------------"
 echo   Shit-Finder is installed successfully!!!!
-<<<<<<< HEAD
 printf "To use shit_finder type 'shit_finder --help'\n\n"
-=======
 printf "To use shit-finder restart your terminal and then type 'findshit --help'\n\n"
->>>>>>> e212361d79d85d7850483d26d4aba3ecb583658a
+
